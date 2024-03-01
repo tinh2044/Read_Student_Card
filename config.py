17554a13e986566d9dbc44007a6614ed22fb0a00
Chi Tinh <tinhnguyen23122004@gmail.com>
2024-03-01 10:02:19 +0700

    config data and model path

---
+++ config.py
@@ -0,0 +1,13 @@
+data_path = {
+    'detect images': "",
+    'detect cards': "./Data/classification_data",
+    'text detection': "",
+    'text recognition': "",
+    'test images': "./Data/test/student_card"
+}
+model_path = {
+    'detect images': "",
+    'detect cards': "",
+    'text detection': "",
+    'text recognition': "",
+    }
